90ede46af4c5774ac7cf2246063e3642a0e1e362
“euridike” <reka.szuecs@icloud.com>
2022-05-18 21:49:42 +0300

    Read me

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-function get_random_color()
-{
-    var color = "";
-    for(var i = 0; i < 3; i++) {
-        var sub = Math.floor(Math.random() * 256).toString(16);
-        color += (sub.length == 1 ? "0" + sub : sub);
-    }
-    return "#" + color;
+function randomColor(brightness) {
+  function randomChannel(brightness) {
+    var r = 255 - brightness;
+    var n = 0 | (Math.random() * r + brightness);
+    var s = n.toString(16);
+    return s.length == 1 ? '0' + s : s;
+  }
+  return (
+    '#' +
+    randomChannel(brightness) +
+    randomChannel(brightness) +
+    randomChannel(brightness)
+  );
 }
-
-function get_rand_color()
-{
-    var color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
-    while(color.length < 6) {
-        color = "0" + color;
-    }
-    return "#" + color;
 //import emoji from 'node-emoji';
 
 //if (process.argv.length < 3) {
